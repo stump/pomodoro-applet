635c21499fc5d512079f95d15e04db0ead057856
John Stumpo <stump@jstump.com>
2010-07-29 03:28:53 -0400

    Add playing of the alarm tone.

--- pomodoro.c
+++ pomodoro.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 #include <panel-applet.h>
 #include <libnotify/notify.h>
+#include <gst/gst.h>
 
 #include <string.h>
 
@@ -15,13 +16,44 @@ struct pom_state {
     POM_BREAK = 2
   } state;
   int seconds_left;
+  GstElement* playbin;
 };
 
-static void pom_notify(const gchar* summary, const gchar* body)
+static void pom_notify(struct pom_state* state, const gchar* summary, const gchar* body, gboolean sound)
 {
   NotifyNotification* note = notify_notification_new(summary, body, NULL, NULL);
   notify_notification_show(note, NULL);
   g_object_unref(note);
+
+  if (sound) {
+    gchar* alarm_tone_filename = g_build_filename(PKGDATADIR, "timerexpired.ogg", NULL);
+    GFile* tonefile = g_file_new_for_path(alarm_tone_filename);
+    g_free(alarm_tone_filename);
+
+    g_object_set(G_OBJECT(state->playbin), "uri", g_file_get_uri(tonefile), NULL);
+    g_object_unref(tonefile);
+    gst_element_set_state(state->playbin, GST_STATE_PLAYING);
+  }
+}
+
+static void pom_gst_message(GstBus* bus, GstMessage* msg, gpointer data)
+{
+  struct pom_state* state = data;
+  GError* err = NULL;
+  gchar* debuginfo = NULL;
+  switch (msg->type) {
+    case GST_MESSAGE_EOS:
+      gst_element_set_state(state->playbin, GST_STATE_NULL);
+      break;
+    case GST_MESSAGE_ERROR:
+      gst_element_set_state(state->playbin, GST_STATE_NULL);
+      gst_message_parse_error(msg, &err, &debuginfo);
+      g_printerr("GStreamer error: %s\n", err->message);
+      g_printerr("Debug info: %s\n", debuginfo);
+      g_error_free(err);
+      g_free(debuginfo);
+      break;
+  }
 }
 
 static void pom_update_label(struct pom_state* state)
@@ -50,11 +82,11 @@ static gboolean pom_second(gpointer data)
       case POM_WORK:
         state->state = POM_BREAK;
         state->seconds_left = BREAK_SECONDS;
-        pom_notify("Pomodoro", "Break time!");
+        pom_notify(state, "Pomodoro", "Break time!", TRUE);
         break;
       case POM_BREAK:
         state->state = POM_STOPPED;
-        pom_notify("Pomodoro", "The break period is over.");
+        pom_notify(state, "Pomodoro", "The break period is over.", TRUE);
         break;
     }
   }
@@ -74,7 +106,7 @@ static gboolean pom_button_pressed(GtkWidget* ebox, GdkEventButton* event, struc
       case POM_WORK:
         state->state = POM_BREAK;
         state->seconds_left = BREAK_SECONDS;
-        pom_notify("Pomodoro", "The current pomodoro was aborted.");
+        pom_notify(state, "Pomodoro", "The current pomodoro was aborted.", FALSE);
         break;
       case POM_BREAK:
         state->state = POM_STOPPED;
@@ -89,6 +121,7 @@ static gboolean pomodoro_applet_fill(PanelApplet* applet, const gchar* iid, gpoi
 {
   struct pom_state* state;
   GtkWidget* ebox;
+  GstBus* bus;
   (void) data;
 
   if (strcmp(iid, "OAFIID:PomodoroApplet") != 0)
@@ -96,6 +129,8 @@ static gboolean pomodoro_applet_fill(PanelApplet* applet, const gchar* iid, gpoi
 
   if (!notify_is_initted())
     notify_init("Pomodoro");
+
+  gst_init(NULL, NULL);
 
   state = g_malloc0(sizeof(struct pom_state));
   state->label = gtk_label_new("Pomodoro");
@@ -105,6 +140,12 @@ static gboolean pomodoro_applet_fill(PanelApplet* applet, const gchar* iid, gpoi
 
   g_signal_connect(G_OBJECT(ebox), "button-press-event", G_CALLBACK(pom_button_pressed), state);
   gtk_widget_show_all(GTK_WIDGET(applet));
+
+  state->playbin = gst_element_factory_make("playbin", NULL);
+  bus = gst_element_get_bus(state->playbin);
+  gst_bus_add_signal_watch(bus);
+  g_signal_connect(G_OBJECT(bus), "message", G_CALLBACK(pom_gst_message), state);
+
   return TRUE;
 }
 
